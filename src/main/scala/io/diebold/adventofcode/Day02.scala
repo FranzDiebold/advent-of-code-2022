f091c6389415e1cbc1db109c79ac8027b31863cb
Franz Diebold <franz.diebold@tum.de>
2022-12-06 22:16:18 +0000

    Add solution for day 2.

---
+++ src/main/scala/io/diebold/adventofcode/Day02.scala
@@ -0,0 +1,112 @@
+package io.diebold.adventofcode
+
+import io.diebold.util.FileReader
+
+object Day02 {
+  enum RockPaperScissorsShape:
+    case Rock, Paper, Scissors
+
+  def decodeFirstColumn(encodedShape: String): RockPaperScissorsShape = {
+    encodedShape match
+      case "A" => RockPaperScissorsShape.Rock
+      case "B" => RockPaperScissorsShape.Paper
+      case "C" => RockPaperScissorsShape.Scissors
+  }
+
+  def readInput(): Iterator[(RockPaperScissorsShape, String)] = {
+    FileReader
+      .readInput(2)
+      .map(_.split(" "))
+      .map((encodedColumns: Array[String]) =>
+        (decodeFirstColumn(encodedColumns(0)), encodedColumns(1))
+      )
+  }
+
+  def getResponseScore(responseShape: RockPaperScissorsShape): Int = {
+    responseShape match
+      case RockPaperScissorsShape.Rock     => 1
+      case RockPaperScissorsShape.Paper    => 2
+      case RockPaperScissorsShape.Scissors => 3
+  }
+
+  def getOutcomeScore(
+      opponentShape: RockPaperScissorsShape,
+      responseShape: RockPaperScissorsShape
+  ): Int = {
+    (opponentShape, responseShape) match
+      case (RockPaperScissorsShape.Rock, RockPaperScissorsShape.Paper)     => 6
+      case (RockPaperScissorsShape.Rock, RockPaperScissorsShape.Scissors)  => 0
+      case (RockPaperScissorsShape.Paper, RockPaperScissorsShape.Rock)     => 0
+      case (RockPaperScissorsShape.Paper, RockPaperScissorsShape.Scissors) => 6
+      case (RockPaperScissorsShape.Scissors, RockPaperScissorsShape.Rock)  => 6
+      case (RockPaperScissorsShape.Scissors, RockPaperScissorsShape.Paper) => 0
+      case _                                                               => 3
+  }
+
+  def getScore(
+      opponentShape: RockPaperScissorsShape,
+      responseShape: RockPaperScissorsShape
+  ): Int = {
+    getResponseScore(responseShape) + getOutcomeScore(opponentShape, responseShape)
+  }
+
+  def solveOne(input: Iterator[(RockPaperScissorsShape, String)]): Int = {
+    def decodeSecondColumn(encodedShape: String): RockPaperScissorsShape = {
+      encodedShape match
+        case "X" => RockPaperScissorsShape.Rock
+        case "Y" => RockPaperScissorsShape.Paper
+        case "Z" => RockPaperScissorsShape.Scissors
+    }
+
+    input
+      .map((opponentShape: RockPaperScissorsShape, encodedResponseShape: String) =>
+        (opponentShape, decodeSecondColumn(encodedResponseShape))
+      )
+      .map(getScore)
+      .sum
+  }
+
+  def partOne(): Int = {
+    solveOne(readInput())
+  }
+
+  def solveTwo(input: Iterator[(RockPaperScissorsShape, String)]): Int = {
+    def getResponseShape(
+        opponentShape: RockPaperScissorsShape,
+        encodedSecondColumn: String
+    ): RockPaperScissorsShape = {
+      // X: loose
+      // Y: draw
+      // Z: win
+      (opponentShape, encodedSecondColumn) match
+        case (RockPaperScissorsShape.Rock, "X")     => RockPaperScissorsShape.Scissors
+        case (RockPaperScissorsShape.Rock, "Z")     => RockPaperScissorsShape.Paper
+        case (RockPaperScissorsShape.Paper, "X")    => RockPaperScissorsShape.Rock
+        case (RockPaperScissorsShape.Paper, "Z")    => RockPaperScissorsShape.Scissors
+        case (RockPaperScissorsShape.Scissors, "X") => RockPaperScissorsShape.Paper
+        case (RockPaperScissorsShape.Scissors, "Z") => RockPaperScissorsShape.Rock
+        case _                                      => opponentShape
+    }
+
+    input
+      .map((opponentShape: RockPaperScissorsShape, encodedSecondColumn: String) =>
+        (opponentShape, getResponseShape(opponentShape, encodedSecondColumn))
+      )
+      .map(getScore)
+      .sum
+  }
+
+  def partTwo(): Int = {
+    solveTwo(readInput())
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Day 2:")
+
+    val resultOne = partOne()
+    println(f"The result for part 1 is: $resultOne")
+
+    val resultTwo = partTwo()
+    println(f"The result for part 2 is: $resultTwo")
+  }
+}
---
+++ src/test/scala/io/diebold/adventofcode/Day02Test.scala
@@ -0,0 +1,15 @@
+package io.diebold.adventofcode
+
+package io.diebold.adventofcode
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day02Test extends AnyFunSuite {
+  test("Day02.partOne") {
+    assert(Day02.partOne() == 15)
+  }
+
+  test("Day02.partTwo") {
+    assert(Day02.partTwo() == 12)
+  }
+}
