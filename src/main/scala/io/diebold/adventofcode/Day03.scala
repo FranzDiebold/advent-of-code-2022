6c09a780a8ff89d97ec5e42fd94a8b06c6df604b
Franz Diebold <franz.diebold@tum.de>
2022-12-07 22:12:30 +0000

    Add solution for day 3.

---
+++ src/main/scala/io/diebold/adventofcode/Day03.scala
@@ -0,0 +1,40 @@
+package io.diebold.adventofcode
+
+import io.diebold.adventofcode.Puzzle
+import io.diebold.util.FileReader
+
+object Day03 extends Puzzle[String, Int] {
+  def readInput(): Iterator[String] = {
+    FileReader
+      .readInput(3)
+  }
+
+  def getPriority(item: Char): Int = item.toInt - (if (item.isUpper) 38 else 96)
+
+  def solveOne(input: Iterator[String]): Int = {
+    input
+      .map((rucksackItems: String) =>
+        (
+          rucksackItems.slice(0, rucksackItems.length / 2),
+          rucksackItems.slice(rucksackItems.length / 2, rucksackItems.length)
+        )
+      )
+      .map((firstCompartmentString: String, secondCompartmentString: String) =>
+        firstCompartmentString.toSet.intersect(secondCompartmentString.toSet)
+      )
+      .map(_.head)
+      .map(getPriority)
+      .sum
+  }
+
+  def solveTwo(input: Iterator[String]): Int = {
+    input
+      .grouped(3)
+      .map((rucksackGroup: Seq[String]) =>
+        rucksackGroup.map(_.toSet).reduceLeft((a: Set[Char], b: Set[Char]) => a.intersect(b))
+      )
+      .map(_.head)
+      .map(getPriority)
+      .sum
+  }
+}
---
+++ src/test/scala/io/diebold/adventofcode/Day03Test.scala
@@ -0,0 +1,15 @@
+package io.diebold.adventofcode
+
+package io.diebold.adventofcode
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day03Test extends AnyFunSuite {
+  test("Day03.partOne") {
+    assert(Day03.partOne() == 157)
+  }
+
+  test("Day03.partTwo") {
+    assert(Day03.partTwo() == 70)
+  }
+}
