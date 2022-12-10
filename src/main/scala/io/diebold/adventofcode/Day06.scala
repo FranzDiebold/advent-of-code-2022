fd4b9265d35bcede551c4fe4fd6be6542f2180cf
Franz Diebold <franz.diebold@tum.de>
2022-12-10 20:47:29 +0000

    Add solutions for day 6.

---
+++ src/main/scala/io/diebold/adventofcode/Day06.scala
@@ -0,0 +1,30 @@
+package io.diebold.adventofcode
+
+import scala.collection.mutable.Stack
+
+import io.diebold.adventofcode.Puzzle
+
+object Day06 extends Puzzle[Seq[Char], Int] {
+  def readInput(): Seq[Char] = {
+    readRawInput().next.toSeq
+  }
+
+  def findMarker(input: Seq[Char], markerSize: Int): Option[Int] = {
+    val marker = input
+      .sliding(markerSize)
+      .zipWithIndex
+      .find((possibleMarkerSeq: Seq[Char], idx: Int) => possibleMarkerSeq.toSet.size == markerSize)
+
+    marker match
+      case Some(actualMarker) => Some(actualMarker(1) + markerSize)
+      case None               => None
+  }
+
+  def solveOne(input: Seq[Char]): Int = {
+    findMarker(input, 4).getOrElse(-1)
+  }
+
+  def solveTwo(input: Seq[Char]): Int = {
+    findMarker(input, 14).getOrElse(-1)
+  }
+}
---
+++ src/test/scala/io/diebold/adventofcode/Day06Test.scala
@@ -0,0 +1,15 @@
+package io.diebold.adventofcode
+
+package io.diebold.adventofcode
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day06Test extends AnyFunSuite {
+  test("Day06.partOne") {
+    assert(Day06.partOne() == 7)
+  }
+
+  test("Day06.partTwo") {
+    assert(Day06.partTwo() == 19)
+  }
+}
